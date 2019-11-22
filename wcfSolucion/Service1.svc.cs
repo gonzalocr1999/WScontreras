119b831fb93cb7acc035ceb801b3de19e86e9457
gonzalocr1999 <gonzalo11_99git config --global user.email gonzalo11_99_@hotmail.com>
2019-11-22 12:36:21 -0500

    Subiendo mi Codigo

---
+++ wcfSolucion/Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcfSolucion
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            string msj;
+            if (value < 18)
+            {
+                msj = " Eres menor de edad , tienes " + value + " años: ";
+            }
+            else if(value >= 18)
+            {
+                msj = "Ya puedes votar en las elecciones!";
+
+            }
+            else
+            {
+                msj = "";
+            }
+
+            return msj;
+            
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
